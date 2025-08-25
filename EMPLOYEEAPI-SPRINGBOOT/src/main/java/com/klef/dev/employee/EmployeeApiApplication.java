d9434a8c0551858db87437a9295cec0120a9039a
Harsh <rajh87897@gmail.com>
2025-08-25 21:32:30 +0530

    Updated pom.xml

--- EMPLOYEEAPI-SPRINGBOOT/src/main/java/com/klef/dev/employee/EmployeeApiApplication.java
+++ EMPLOYEEAPI-SPRINGBOOT/src/main/java/com/klef/dev/employee/EmployeeApiApplication.java
@@ -2,9 +2,15 @@ package com.klef.dev.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeApiApplication {
+public class EmployeeApiApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmployeeApiApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApiApplication.class, args);
     }
